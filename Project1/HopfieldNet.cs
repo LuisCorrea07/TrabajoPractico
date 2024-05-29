537344d224671dec2019b65c2ca5b7dc71d8ddd8
Luis Emilio Correa Díaz <luiscorrea070192@gmail.com>
2024-05-28 22:39:23 -0300

    Add files via upload
    En esta Rama se trata el trabajo practico Nº3 donde se pide hacer un algoritmo de Hopfield para imagenes

---
+++ Project1/HopfieldNet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+
+
+namespace Project1
+{
+    class HopfieldNet
+    {
+        private int[,] weights;
+        private int size;
+
+        public HopfieldNet(int size)
+        {
+            this.size = size;
+            this.weights = new int[size, size];
+        }
+
+        // Entrenamiento de la red
+        public void Train(int[] patron)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i != j)
+                    {
+                        weights[i, j] += patron[i] * patron[j];
+                    }
+                }
+            }
+        }
+
+        // Actualización de la red
+        public int[] Update(int[] patron)
+        {
+            int[] nuevoPatron = new int[size];
+            patron.CopyTo(nuevoPatron, 0);
+
+            for (int i = 0; i < size; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    sum += weights[i, j] * nuevoPatron[j];
+                }
+                nuevoPatron[i] = sum >= 0 ? 1 : -1;
+            }
+            return nuevoPatron;
+        }
+    }
+}
